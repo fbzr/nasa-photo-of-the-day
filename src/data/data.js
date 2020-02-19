05b4d27d894e0207d8d3140d2fad2cff9f451a36
fbzr <fabricio.spb@gmail.com>
2020-02-19 08:31:00 -0800

    fix data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default async function fetchData = () => {
+export default async function fetchData() {
     const res = await axios.get('https://api.nasa.gov/planetary/apod?api_key=VPDBBijjACBup4d37wNWuMYSbCnepqynSIHPVGOm');
     console.log(res);
 }
